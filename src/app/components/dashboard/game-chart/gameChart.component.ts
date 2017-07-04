7e3deb2f6cb88a1c80815af9e8e85f357a19e6cb
Violaine FABRY <violaine.fabry@niji.fr>
2017-07-04 17:13:08 +0200

    Dashboard: add real games to chart component

--- src/app/components/dashboard/game-chart/gameChart.component.ts
+++ src/app/components/dashboard/game-chart/gameChart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
     moduleId: module.id.replace("/dist/", "/"),
@@ -6,81 +6,66 @@ import { Component } from '@angular/core';
     templateUrl: './game-chart.html'
 })
 
-export class GameChartComponent {
+export class GameChartComponent implements OnChanges {
+  @Input() games: any;
+  public lineChartData: Array<any> = [];
+  public lineChartLabels:Array<any> = [];
 
-    scores = [
-        2120,
-        null,
-        null,
-        2400,
-        2400,
-        2300,
-        2150,
-        2200,
-        2230,
-        2200,
-        2210,
-        2210
-    ]
-
-    public lineChartOptions:any = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    max: this.getMax(),
-                    min: this.getMin(),
-                }
-            }]
-        },
-        tooltips: {
-            callbacks: {
-                label: this.tooltipLabel
-            }
-        },
-        responsive: true
-    };
-
-    public lineChartData: Array<any> = [
-        {data: this.scores, label: 'Season 4', lineTension: 0.2, spanGaps: false}
-
-    ]
-
-    public lineChartLabels:Array<any> = [
-        18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29
-    ]
-
-    public lineChartColors:Array<any> = [
-        { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-        }
-    ]
-
-    tooltipLabel(tooltipItem:any, data:any) {
-        console.log(tooltipItem);
-        let result:string = "Score: "+tooltipItem.xLabel+"\n";
-        if(this.scores[tooltipItem.index-1] !== null) {
-            let value = tooltipItem.xLabel-this.scores[tooltipItem.index-1];
-            result += "";
-        }
+  ngOnChanges(changes: SimpleChanges) {
+      if(changes["games"]) {
+        let labels = this.games.map((game: any) => game.game_id));
+        let data = this.games.map((game:any) => game.rating ));
+        this.lineChartLabels = labels.reverse();
+        this.lineChartData = [{
+          data: data.reverse(),
+          label: "Season "+this.games[0].seasons_id,
+          lineTension: 0.2,
+          spanGaps: false
+        }];
     }
+  }
 
-    getMax() {
-        return Math.max.apply(null, this.scores) + 50;
-    }
+  public lineChartOptions:any = {
+      // tooltips: {
+      //     callbacks: {
+      //         label: this.tooltipLabel
+      //     }
+      // },
+      responsive: true
+  };
 
-    getMin() {
-        let result = 3000;
-        for(let score of this.scores) {
-            if (score !== null && score < result) {
-                result = score;
-            }
-        }
-        return result - 50;
-    }
+  public lineChartColors:Array<any> = [
+      {
+          backgroundColor: 'rgba(0,0,0,0)',
+          borderColor: '#9542f4',
+          pointBackgroundColor: '#6d27bc',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: '#6d27bc'
+      }
+  ]
+
+  // tooltipLabel(tooltipItem:any, data:any) {
+  //     console.log(tooltipItem);
+  //     let result:string = "Score: "+tooltipItem.xLabel+"\n";
+  //     if(this.scores[tooltipItem.index-1] !== null) {
+  //         let value = tooltipItem.xLabel-this.scores[tooltipItem.index-1];
+  //         result += "";
+  //     }
+  // }
+  //
+  // getMax() {
+  //     return Math.max.apply(null, this.scores) + 50;
+  // }
+  //
+  // getMin() {
+  //     let result = 3000;
+  //     for(let score of this.scores) {
+  //         if (score !== null && score < result) {
+  //             result = score;
+  //         }
+  //     }
+  //     return result - 50;
+  // }
 
 }
