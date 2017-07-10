8533df5ea87e5f77538a6a96e66cb8463b6f9496
Violaine FABRY <violaine.fabry@niji.fr>
2017-07-10 18:06:27 +0200

    Dashboard: gamesDetails v0.1

--- src/app/components/dashboard/games-details/gamesDetails.component.ts
+++ src/app/components/dashboard/games-details/gamesDetails.component.ts
@@ -1,20 +1,51 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+import { DataService } from './../../../services/api/data.service';
 
 @Component({
     moduleId: module.id.replace("/dist/", "/"),
     selector: 'games-details',
     templateUrl: './games-details.html',
-    // styleUrls: [ './games-details.css']
+    providers: [ DataService ],
+    styleUrls: [ './games-details.css']
 })
 
-export class GamesDetailsComponent implements OnChanges {
+export class GamesDetailsComponent implements OnChanges, OnInit {
   @Input() games:any;
-  lastGamesInfos: {rating: number, variation: number, outcome: string }[];
+  maps: any = [];
+  heroes: any = [];
+  selectedMap: any;
+  addGameId: number;
+  isValid: boolean = false;
+  newRank: string;
+
+  constructor (private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getMaps()
+    .subscribe(
+      maps =>  { this.maps = maps; this.selectedMap = maps[0]; },
+      error => console.log(error.toString())
+    )
+    this.dataService.getHeroes()
+    .subscribe(
+      heroes =>  { this.heroes = heroes },
+      error => console.log(error.toString())
+    )
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if(changes['games']) {
-      console.log(this.games);
-      this.lastGamesInfos = this.games.slice(0,4);
+      console.table(this.games);
+      this.addGameId = this.games[0] + 1;
     }
+  }
+
+  newRankChange() { //FIXME
+    if (Number(this.newRank) !== NaN) {
+      this.isValid = Number(this.newRank) < 5000;
+    }
+    this.isValid = false;
+    console.log(Number(this.newRank));
   }
 }
