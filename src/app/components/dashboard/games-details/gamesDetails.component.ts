b43ee3ade9e3fbc74aeb65ce1587a22712a64cab
Violaine <violaine.fabry@insa-rennes.fr>
2017-07-17 15:00:00 +0200

    Dashboad: add function to add new games, do not include heroes

--- src/app/components/dashboard/games-details/gamesDetails.component.ts
+++ src/app/components/dashboard/games-details/gamesDetails.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 import { DataService } from './../../../services/api/data.service';
+import { GamesService } from './../../../services/api/games.service';
 
 declare var tippy: any;
 
@@ -8,71 +9,86 @@ declare var tippy: any;
     moduleId: module.id.replace("/dist/", "/"),
     selector: 'games-details',
     templateUrl: './games-details.html',
-    providers: [ DataService ],
+    providers: [ DataService, GamesService ],
     styleUrls: [ './games-details.css']
 })
 
 export class GamesDetailsComponent implements OnChanges, OnInit {
-  @Input() games:any;
-  maps: any = [];
-  heroes: any = [];
-  selectedMap: any;
-  addGameId: number;
-  isValid: boolean = true;
-  newRank: string;
-  // heroesByType = [];
-  test = "Hello world";
+    @Input() games:any;
+    @Input() playerId: string;
+    maps: any = [];
+    heroes: any = [];
+    selectedMap: any;
+    newGameNb: number;
+    isValid: boolean = true;
+    newRank: string;
+    // heroesByType = [];
+    test = "Hello world";
 
-  constructor (private dataService: DataService) {}
+    constructor (
+        private dataService: DataService,
+        private gamesService: GamesService) {}
 
-  ngOnInit() {
-    tippy("#tooltip", {
-      html: document.querySelector('#tooltip-template'),
-      trigger: 'click',
-      interactive: true,
-      position: 'top',
-      animation: 'scale',
-      duration: 500,
-      arrow: true,
-      theme: 'light'
-    });
+    ngOnInit() {
+        tippy("#tooltip", {
+            html: document.querySelector('#tooltip-template'),
+            trigger: 'click',
+            interactive: true,
+            position: 'top',
+            animation: 'scale',
+            duration: 500,
+            arrow: true,
+            theme: 'light'
+        });
 
-    this.dataService.getMaps()
-    .subscribe(
-      maps =>  { this.maps = maps },
-      error => console.log(error.toString())
-    )
-    // this.dataService.getHeroes()
-    // .subscribe(
-    //   heroes =>  { this.heroes = heroes; this.initHeroes(); },
-    //   error => console.log(error.toString())
-    // )
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if(changes['games'] && this.games.length > 0) {
-    //   console.table(this.games);
-      this.addGameId = this.games[0] + 1;
+        this.dataService.getMaps()
+        .subscribe(
+            maps =>  { this.maps = maps },
+            error => console.log(error.toString())
+        )
+        // this.dataService.getHeroes()
+        // .subscribe(
+        //   heroes =>  { this.heroes = heroes; this.initHeroes(); },
+        //   error => console.log(error.toString())
+        // )
     }
-  }
 
-  addGame() {
-    if (Number(this.newRank) === NaN || Number(this.newRank) > 5000) {
-      this.isValid = false;
+    ngOnChanges(changes: SimpleChanges) {
+        if(changes['games'] && this.games.length > 0) {
+            //   console.table(this.games);
+            this.newGameNb = this.games[0] + 1;
+        }
+        else {
+            this.newGameNb = 1;
+        }
     }
-    else {
-      this.isValid = true;
-      //TODO post game
-    }
-  }
 
-  // initHeroes() {
-  //   for(let hero of this.heroes) {
-  //     if (!this.heroesByType.hasOwnProperty(hero.type)) {
-  //       this.heroesByType[hero.type] = [];
-  //     }
-  //     this.heroesByType[hero.type].push(hero.name);
-  //   }
-  //   console.table(this.heroesByType);
-  // }
+    addGame() {
+        if (Number(this.newRank) === NaN || Number(this.newRank) > 5000) {
+            this.isValid = false;
+        }
+        else {
+            this.isValid = true;
+            this.gamesService.postGame(
+                this.newGameNb,
+                this.playerId,
+                Number(this.newRank),
+                this.selectedMap
+            ).subscribe(
+                response => console.log(response),
+                error => console.error(error)
+            )
+
+        }
+    }
+
+    // initHeroes() {
+    //   for(let hero of this.heroes) {
+    //     if (!this.heroesByType.hasOwnProperty(hero.type)) {
+    //       this.heroesByType[hero.type] = [];
+    //     }
+    //     this.heroesByType[hero.type].push(hero.name);
+    //   }
+    //   console.table(this.heroesByType);
+    // }
 }
--- src/app/services/api/games.service.ts
+++ src/app/services/api/games.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Headers, Http, Response, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
@@ -7,19 +7,40 @@ import 'rxjs/add/operator/map'
 
 @Injectable()
 export class GamesService {
-  private gamesUrl: string = "https://owi.b00.biz";
-  //92e4c6ee-b76f-410d-b657-fd640a1aa5ae
+    private gamesUrl: string = "https://owi.b00.biz";
+    //92e4c6ee-b76f-410d-b657-fd640a1aa5ae
 
-  constructor (private http: Http) {}
+    constructor (private http: Http) {}
 
-  getFullInfoGames(player_id: string) {
-    let url = this.gamesUrl + "/players/" + player_id +"/games";
-    return this.http.get(url)
-                    .map(response => response.json() || {})
-                    .catch(this.handleError);
-  }
+    getFullInfoGames(player_id: string) {
+        let url = this.gamesUrl + "/players/" + player_id +"/games";
+        return this.http.get(url)
+            .map(response => response.json() || {})
+            .catch(this.handleError);
+    }
 
-  handleError(error : Response | any) {
-    return error.message ? error.message : error.toString();
-  }
+    postGame(gameId: number, playerId: string, rating: number, mapId: string ) {
+        let game = {
+            "game_id": gameId,
+            "player_id": playerId,
+            "rating": rating,
+            "map_id": mapId
+        }
+        let headers = new Headers({
+            "Content-Type": "application/json",
+            "Prefer": "return=representation",
+            "Accept": "application/json"
+        });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(
+            this.gamesUrl+"/games",
+            JSON.stringify(game),
+            options)
+            .map(response => response.json())
+            .catch(error => this.handleError(error));
+    }
+
+    handleError(error : Response | any) {
+        return error.message ? error.message : error.toString();
+    }
 }
