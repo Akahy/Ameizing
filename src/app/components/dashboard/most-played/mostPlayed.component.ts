60e683fd1762bb087112cb06da584ed83688cf7a
Violaine <violaine.fabry@insa-rennes.fr>
2017-08-10 18:26:14 +0200

    Dashboard: most played heroes 1st design

--- src/app/components/dashboard/most-played/mostPlayed.component.ts
+++ src/app/components/dashboard/most-played/mostPlayed.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+import { OverwatchService } from './../../../services/api/overwatch.service';
 
 @Component({
     selector: 'most-played',
     templateUrl: './most-played.html',
-    // styleUrls: [ './most-played.css']
+    providers: [ OverwatchService ],
+    styleUrls: [ './most-played.css']
 })
 
-export class MostPlayedComponent {
+export class MostPlayedComponent implements OnChanges {
+    @Input() tag: string;
+    playtimes: any[] = [];
+
+    constructor ( private overwatchService: OverwatchService ) {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        if(changes['tag']) {
+            this.playtimes = [];
+            this.getPlaytime();
+        }
+    }
+
+    sortPlaytime(playtime: any) {
+        this.playtimes = Object.keys(playtime)
+            .map(key => {
+                return {hero: key, time: Math.round((playtime[key] + 0.00001) * 10) / 10};
+            })
+            .sort((a,b) => b.time - a.time)
+            .slice(0,6);
+    }
+
+    getPlaytime() {
+        this.overwatchService.getHeroesInformations(this.tag)
+            .subscribe(
+                res => this.sortPlaytime(res.eu.heroes.playtime.competitive),
+                error => console.error(error)
+            );
+    }
 }
