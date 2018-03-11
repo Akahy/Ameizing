199b75f53cd231d0aa9709dc5b97b2a5f851bd3c
Violaine Fabry <vio-violaine@hotmail.fr>
2018-03-11 12:01:57 +0100

    Replace deprecated Http by HttpClient and update some visual content

--- src/app/services/api/games.service.ts
+++ src/app/services/api/games.service.ts
@@ -1,30 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http, Response, RequestOptions } from '@angular/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class GamesService {
     private gamesUrl: string = "https://owi.b00.biz";
     //92e4c6ee-b76f-410d-b657-fd640a1aa5ae
 
-    constructor (private http: Http) {}
+    constructor (private http: HttpClient) {}
 
     getFullInfoGames(player_id: string) {
         let url = this.gamesUrl + "/players/" + player_id +"/games";
         return this.http.get(url)
-            .map(response => response.json() || {})
-            .catch(this.handleError);
+            .pipe(
+                catchError((error) => {
+                    console.error(error);
+                    return new ErrorObservable("Whoooops!");
+                })
+            );
     }
 
     getLastGame(playerId: string) {
         let url = this.gamesUrl + "/players/" + playerId +"/games?limit=1";
         return this.http.get(url)
-            .map(response => response.json() || {})
-            .catch(this.handleError);
+            .pipe(
+                catchError((error) => {
+                    console.error(error);
+                    return new ErrorObservable("Whoooops!");
+                })
+            );
     }
 
     postGame(gameId: number, playerId: string, rating: number, mapId: string ) {
@@ -34,18 +41,22 @@ export class GamesService {
             "rating": rating,
             "map_id": mapId
         }
-        let headers = new Headers({
-            "Content-Type": "application/json",
-            "Prefer": "return=representation",
-            "Accept": "application/json"
-        });
-        let options = new RequestOptions({ headers: headers });
-        return this.http.post(
-            this.gamesUrl+"/games",
-            JSON.stringify(game),
-            options)
-            .map(response => response.json())
-            .catch(error => this.handleError(error));
+
+        let url = this.gamesUrl+"/games";
+        const options = {
+            headers:  new HttpHeaders({
+                "Content-Type": "application/json",
+                "Prefer": "return=representation",
+                "Accept": "application/json"
+            })
+        };
+        return this.http.post(url, JSON.stringify(game), options)
+            .pipe(
+                catchError((error) => {
+                    console.error(error);
+                    return new ErrorObservable("Whoooops!");
+                })
+            );
     }
 
     handleError(error : Response | any) {
@@ -60,17 +71,21 @@ export class GamesService {
                 "hero_id": hero
             });
         }
-        let headers = new Headers({
-            "Content-Type": "application/json",
-            "Prefer": "return=representation",
-            "Accept": "application/json"
-        });
-        let options = new RequestOptions({ headers: headers });
-        return this.http.post(
-            this.gamesUrl+"/game_has_heros",
-            JSON.stringify(info),
-            options)
-            .map(response => response.json())
-            .catch(error => this.handleError(error));
+
+        let url = this.gamesUrl+"/game_has_heros";
+        const options = {
+            headers:  new HttpHeaders({
+                "Content-Type": "application/json",
+                "Prefer": "return=representation",
+                "Accept": "application/json"
+            })
+        };
+        return this.http.post(url, JSON.stringify(info), options)
+            .pipe(
+                catchError((error) => {
+                    console.error(error);
+                    return new ErrorObservable("Whoooops!");
+                })
+            );
     }
 }
